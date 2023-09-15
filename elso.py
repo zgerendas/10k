e60262bcffde41de0a06116c92036f0ce7d45ac2
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2023-09-15 12:30:14 +0200

    elso

---
+++ elso.py
@@ -0,0 +1,47 @@
+'''
+print("Helló világ!")
+#print("Szia" + 12)     szöveg + szám HIBA
+#print("Szia
+#      Pista!")
+
+print("""Szia
+Pista!""")
+print('-'*10)
+print("Szia")
+print("Pista!")
+
+print('-'*10)
+print("Szia",end=" ")
+print("Pista!")
+print("Szia",end="......")
+print("Pista!")
+print(1,2,3,4,5,6)
+print(1,',',2,',',3,4,5,6)
+print(1,2,3,4,5,6,sep=", ")
+'''
+print("Szia\aPista")    # ???
+print("b Szia\bPista")    # back space
+print("f Szia\fPista")
+print("n Szia\nPista")    # uj sor
+print("r Szia123456\rPista")
+print("t Szia\tPista")    
+print("v Szia\vPista")    
+print("Szia\\Pista")
+print("Szia\"Pista")
+print('Szia\'Pista')
+print("-"*10)
+
+x = 12
+y = 23
+print(x,y,x * y)
+
+x = x ** 2
+print(x,y,x * y)
+
+x = x + 1
+print(x,y,x * y)
+
+x += 1
+print(x,y,x * y)
+
+
