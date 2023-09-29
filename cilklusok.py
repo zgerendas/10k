46a8122c1a35f7b50c568e4390be0300915fe407
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2023-09-29 11:30:34 +0200

    ciklusok0

--- cilklusok.py
+++ cilklusok.py
@@ -1,1 +1,61 @@
+'''
 szamok = [1,2,5,6,9]
+for szam in szamok:
+    print(szam)
+    x = szam
+    x += 10
+print(x)
+
+# elemek = {"alma",12,34,'k',[1,2]} - halmazban nem lehet lista
+elemek = ("alma",12,34,'k',[1,2]) #  tuple
+for x in elemek:
+    print(type(x),x)
+
+elemek = ["alma",12,34,'k',[1,2]] #  lista
+for x in elemek:
+    print(type(x),x)
+
+#for (i = 1 ; i < 10; i++) {}
+
+for i in range(1,11):
+    #print(i,i*i)    # i**2 == i*i
+    print(i,i*i,sep="^2 = ")
+
+
+for i in range(2,101,2):
+    #print(i,i*i)    # i**2 == i*i
+    print(i,i*i*i,sep="^3 = ")
+
+'''
+'''
+for i in range(10,-11,-3):
+    print(i)
+
+for x in ['a','b']:
+        print(x)
+for x in ['a','b']:
+ print(x)
+ print(x*2)
+
+
+for x in ['a','b']:
+#        print(x)
+    None
+for x in ['a','b']:
+ print(x)
+ print(x*2)
+'''
+lista=[1,23,44,5,8,12]
+x=0
+for e in lista:
+    print(x,e)
+    x +=1
+
+print(len(lista))
+print(lista[0],lista[5])
+# print(lista[6])
+print('-'*20)
+for i in range(len(lista)):
+    print(i,lista[i])
+
+
